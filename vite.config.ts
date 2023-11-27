52449828e9a0f137c44a545d42befeb692ddcb55
sundung <sun1358742@163.com>
2023-11-28 03:37:49 +0800

    解决 vite + ts 使用 postcss-px-to-viewport 报错 找不到声明文件

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import Components from 'unplugin-vue-components/vite';
 import { VantResolver } from '@vant/auto-import-resolver';
-import postcsspxtoviewport from 'postcss-px-to-viewport'
+import postcsspxtoviewport from "postcss-px-to-viewport" //插件
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
