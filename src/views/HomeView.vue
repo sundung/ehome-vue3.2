c44ce5ca8ee0249b7282ae4e1472e9e6abd0026c
sundung <sun1358742@163.com>
2023-11-28 18:06:20 +0800

    webstorm中配置eslint+prettier

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,21 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
     <!--    导航栏-->
     <van-nav-bar title="e 到家" />
-
-
   </main>
 </template>
 
-<style scoped  lang="scss">
+<style scoped lang="scss">
 //  改变导航栏背景颜色
 .van-nav-bar {
-  background-color:$van_nav_bar_background;
+  background-color: $van_nav_bar_background;
 
-  ::v-deep(.van-nav-bar__content)  {
+  ::v-deep(.van-nav-bar__content) {
     .van-nav-bar__title {
-      color:$van_nav_bar_text_color;
+      color: $van_nav_bar_text_color;
     }
   }
-
 }
 </style>
