79f3b827279d8631d1546814cfed856464b45068
sundung <sun1358742@163.com>
2023-11-28 18:50:54 +0800

    首页轮播图,主菜单区域静态完成

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-
 import 'normalize.css'
+import '@/assets/css/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 // 引入 vant 组件库
-import { Button,NavBar } from 'vant';
+import { Button, NavBar, Swipe, SwipeItem } from 'vant'
 import App from './App.vue'
 import router from './router'
 
@@ -13,5 +13,7 @@ app.use(createPinia())
 app.use(router)
 app.use(Button)
 app.use(NavBar)
+app.use(Swipe)
+app.use(SwipeItem)
 
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,17 +4,84 @@
   <main>
     <!--    导航栏-->
     <van-nav-bar title="e 到家" />
+    <!--    轮播图-->
+    <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+      <van-swipe-item>1</van-swipe-item>
+      <van-swipe-item>2</van-swipe-item>
+      <van-swipe-item>3</van-swipe-item>
+      <van-swipe-item>4</van-swipe-item>
+    </van-swipe>
+    <!--    主菜单区域-->
+    <div class="mainMenu">
+      <div class="item">
+        <img src="@/assets/image/mainMenu/jiadianweixiu.png" alt="" />
+        <text>家电维修</text>
+      </div>
+      <div class="item">
+        <img src="@/assets/image/mainMenu/jiadianqingxi.png" alt="" />
+        <text>家电清洗</text>
+      </div>
+      <div class="item">
+        <img src="@/assets/image/mainMenu/jiadiananzhuang.png" alt="" />
+        <text>家电安装</text>
+      </div>
+      <div class="item">
+        <img src="@/assets/image/mainMenu/guandaoshutong.png" alt="" />
+        <text>管道疏通</text>
+      </div>
+      <div class="item">
+        <img src="@/assets/image/mainMenu/shuiluweixiu.png" alt="" />
+        <text>水路维修</text>
+      </div>
+      <div class="item">
+        <img src="@/assets/image/mainMenu/dianluweixiu.png" alt="" />
+        <text>电路维修</text>
+      </div>
+    </div>
   </main>
 </template>
 
 <style scoped lang="scss">
-//  改变导航栏背景颜色
-.van-nav-bar {
-  background-color: $van_nav_bar_background;
+main {
+  //  改变导航栏背景颜色
+  .van-nav-bar {
+    background-color: $van_nav_bar_background;
 
-  ::v-deep(.van-nav-bar__content) {
-    .van-nav-bar__title {
-      color: $van_nav_bar_text_color;
+    ::v-deep(.van-nav-bar__content) {
+      .van-nav-bar__title {
+        color: $van_nav_bar_text_color;
+      }
+    }
+  }
+  //轮播图区域
+  .my-swipe {
+    .van-swipe-item {
+      color: #fff;
+      font-size: 20px;
+      line-height: 150px;
+      text-align: center;
+      background-color: #39a9ed;
+    }
+  }
+  //  主菜单区域
+  .mainMenu {
+    height: 188px;
+    background-color: #fff;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    .item {
+      width: 33vw;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      font-size: 12px;
+      img {
+        width: 40px;
+        height: 40px;
+        margin-bottom: 10px;
+      }
     }
   }
 }
